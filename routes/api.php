b554c0fcf5fe9008dfd834a5c4ee552fbbaf9e3c
lucasfbr <lucas-fbr@hotmail.com>
2024-05-21 13:56:50 -0300

    Model Company

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,11 @@
 
 use App\Http\Controllers\Api\{
     CategoryController,
-    #CompanyController
+    CompanyController
 };
 use Illuminate\Support\Facades\Route;
 
-#Route::apiResource('companies', CompanyController::class);
+Route::apiResource('companies', CompanyController::class);
 Route::apiResource('categories', CategoryController::class);
 
 // Route::get('/', function () {
